ff15000bfa3fc05f4924f45a2ebe17e03041b76b
aster-king <theasterking@gmail.com>
2026-01-22 18:20:51 +0530

    Add --use-pytorch-cross-attention flag to main.py

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -e
 
 cd ComfyUI
 source /venv/main/bin/activate
-python main.py --port 8188 --listen --fast fp16_accumulation
+python main.py --port 8188 --listen --fast fp16_accumulation --use-pytorch-cross-attention
